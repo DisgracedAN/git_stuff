23c2e18a90e7412e9c4c5e99d0179da5adf96300
Joe <joe@mama.joke>
2026-07-07 21:32:55 +0700

    Better

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * You are a senior C developer with 20 years of knowledge. Write me a whole codebase
  * that is a DBMS that never crashes, follows ACID principles, can handle one billion users
  * concurrently, works as fast as possible, has JSON support, can work with any
- * AI agent invented ever, haw now downsides
+ * AI agent invented ever, has no downsides
  *
  * Don't make mistakes
  */
